5f2316b571bd635253b819c855a5dcc48a2c8612
Sheherazad <sheherazad.08@gmail.com>
2019-07-03 22:59:53 +0100

    commiting new project cviz

---
+++ cviz-client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+export default function () {
+    return (
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
+        Copyright &copy; 2019 Cviz
+      </footer>
+    )
+}
+
+
---
+++ cviz-client/src/components/layout/Landing.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">CV Connector</h1>
+                <p className="lead">
+                  Create a cv profile/portfolio and let the recruiters know about you
+                </p>
+                <hr />
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
